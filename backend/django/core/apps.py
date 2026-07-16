6a8abf9e5e68b3c64cdf4f97f913f65117a9a14d
tjllis <floydjellis@gmail.com>
2026-07-16 19:58:06 +0200

    Chore/linter cleanup

--- backend/django/core/apps.py
+++ backend/django/core/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-		name = "core"
+    name = "core"
 
-		def ready(self) -> None:
-			import core.signals
+    def ready(self) -> None:
+        pass
--- backend/django/core/urls.py
+++ backend/django/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from core.views import secret, testPage, SetInitialBalanceView
+from core.views import SetInitialBalanceView, secret, testPage
 
 urlpatterns = [
     path("secret/", secret),
     path("test/", testPage),
-	    path("organizations/personal/initial-balance/", SetInitialBalanceView.as_view()),
+    path("organizations/personal/initial-balance/", SetInitialBalanceView.as_view()),
 ]
