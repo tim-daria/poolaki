f8e7b831d1576f837b76d167c74307f3b40e138f
mainibet <mainibet@gmail.com>
2026-07-25 18:40:39 +0200

    Fix(Docker): change sudo and creation of venv only for root user

--- backend/django/entrypoint.sh
+++ backend/django/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo "Running migrations..."
-uv run python manage.py makemigrations
-uv run python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 echo "Creating Admin account..."
 DJANGO_SUPERUSER_USERNAME="$DJANGO_SUPERUSER_USERNAME" \
 DJANGO_SUPERUSER_EMAIL="admin@poolaki.localhost" \
 DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
-uv run python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 
 echo "Linking admin email to allauth..."
-uv run python manage.py shell -c "
+python manage.py shell -c "
 from django.contrib.auth import get_user_model
 from allauth.account.models import EmailAddress
 User = get_user_model()
@@ -20,4 +20,4 @@ for user in User.objects.all():
 "
 
 echo "Starting server..."
-uv run python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
