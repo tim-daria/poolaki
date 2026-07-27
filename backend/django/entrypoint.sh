4f5601365d8a25fb616bde47dff40f3ed0700cfc
mainibet <mainibet@gmail.com>
2026-07-27 17:19:41 +0200

    Remove unnecessary part from the entrypoint

--- backend/django/entrypoint.sh
+++ backend/django/entrypoint.sh
@@ -10,14 +10,5 @@ DJANGO_SUPERUSER_EMAIL="admin@poolaki.localhost" \
 DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
 python manage.py createsuperuser --no-input
 
-echo "Linking admin email to allauth..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-from allauth.account.models import EmailAddress
-User = get_user_model()
-for user in User.objects.all():
-    EmailAddress.objects.get_or_create(user=user, email=user.email, defaults={'primary': True, 'verified': True})
-"
-
 echo "Starting server..."
 exec python manage.py runserver 0.0.0.0:8000
