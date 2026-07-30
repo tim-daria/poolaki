d74ee7d9db61d29bdd0c173c617733439b12a4f9
tim-daria <darya.timofeeva.97@gmail.com>
2026-07-30 11:20:58 +0200

    tests: add postman test for the new API

--- backend/django/entrypoint.sh
+++ backend/django/entrypoint.sh
@@ -5,9 +5,9 @@ uv run python manage.py makemigrations
 uv run python manage.py migrate
 
 echo "Creating Admin account..."
-DJANGO_SUPERUSER_USERNAME="$DJANGO_SUPERUSER_USERNAME" \
-DJANGO_SUPERUSER_EMAIL="admin@poolaki.localhost" \
-DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
+# DJANGO_SUPERUSER_USERNAME="$DJANGO_SUPERUSER_USERNAME" \
+# DJANGO_SUPERUSER_EMAIL="admin@poolaki.localhost" \
+# DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
 uv run python manage.py createsuperuser --no-input
 
 echo "Starting server..."
