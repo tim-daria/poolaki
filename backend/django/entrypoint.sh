34aecc1cb35a4f6c75f07b5d9ed46dba1849c966
Spanxx <Spanxx89@gmail.com>
2026-07-31 14:31:19 +0200

    Removed unneccessary env variables from entrypoin script

--- backend/django/entrypoint.sh
+++ backend/django/entrypoint.sh
@@ -5,9 +5,6 @@ python manage.py makemigrations
 python manage.py migrate
 
 echo "Creating Admin account..."
-DJANGO_SUPERUSER_USERNAME="$DJANGO_SUPERUSER_USERNAME" \
-DJANGO_SUPERUSER_EMAIL="admin@poolaki.localhost" \
-DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
 python manage.py createsuperuser --no-input
 
 echo "Starting server..."
