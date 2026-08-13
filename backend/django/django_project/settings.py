91fb60b7faa25c4c1f14a0f17263df9a2eec40f0
Spanxx <Spanxx89@gmail.com>
2026-08-13 13:29:29 +0200

    added cloudflare to docker-compose.ci.yml and uncommented localhost:5173 in django settings.

--- backend/django/django_project/settings.py
+++ backend/django/django_project/settings.py
@@ -46,15 +46,15 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://poolaki.localhost",
     "https://poolaki.de",
-    # "http://localhost:5173",
-    # "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://poolaki.localhost",
     "https://poolaki.de",
-    # "http://localhost:5173",
-    # "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CSRF_COOKIE_HTTPONLY = False
