b1aba749db09f3dc37cca8a4bb84f2618269348b
mainibet <mainibet@gmail.com>
2026-08-17 13:36:56 +0200

    Style(notification): adapt notification with the backend documentation

--- frontend/src/context/NotificationContext.ts
+++ frontend/src/context/NotificationContext.ts
@@ -1,19 +1,19 @@
 import { createContext } from "react"
 
-/** Later we can include also transactions, goals, if a member left */
-export type NotificationType = "invitation";
-
 export type InvitationPayload = {
   invitation_id: number;
-  org_name: string;
   invited_by: string;
 };
+
+/** Later we can include also transactions, goals, if a member left */
+export type NotificationType = "invitation";
 
 export type Notification = {
   id: number;
   type: NotificationType;
+  org_id: number;
+  org_name: string;
   payload: InvitationPayload | Record<string, unknown>;
   is_read: boolean;
   created_at: string;
-  org: number | null;
 };
